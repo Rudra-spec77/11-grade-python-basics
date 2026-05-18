5cd70f275c5ab5a0383ed76a831a6dc49fb1d5f3
Rudra-spec77 <dslasher021@gmail.com>
2026-05-18 16:26:47 +0530

    Update MINI CALCULATOR.py

--- MINI CALCULATOR.py
+++ MINI CALCULATOR.py
@@ -3,13 +3,13 @@ num2=int(input("enter no.2="))
 op=input("enter operator")
 if op=='+':
     print(num1+num2)
-if op=='-':
+elif op=='-':
     print(num1-num2)
-if op=='*':
+elif op=='*':
     print(num1*num2)
-if op=='/':
+elif op=='/':
     print(num1/num2)
-if op=='**':
+elif op=='**':
     print(num1**num2)
 else:
     print('invalid input')
