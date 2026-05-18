60cd18efc274caa2d2e3a644a7dcf5b24f6920f2
Rudra-spec77 <dslasher021@gmail.com>
2026-05-18 15:46:49 +0530

    Created a Mini Calculator

---
+++ MINI CALCULATOR.py
@@ -0,0 +1,15 @@
+num1=int(input("enter no.1="))
+num2=int(input("enter no.2="))
+op=input("enter operator")
+if op=='+':
+    print(num1+num2)
+if op=='-':
+    print(num1-num2)
+if op=='*':
+    print(num1*num2)
+if op=='/':
+    print(num1/num2)
+if op=='**':
+    print(num1**num2)
+else:
+    print('invalid input')
